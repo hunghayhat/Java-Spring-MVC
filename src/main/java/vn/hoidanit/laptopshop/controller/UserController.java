046eb91e514486dcb61c71b1b7ade975e75b593b
Hung Hay Hat <diegocoshung19@gmail.com>
2025-02-04 11:03:39 +0700

    add static folder, render html file

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,24 +5,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RestController
-public class UserController {
+// @RestController
+// public class UserController {
 
-    //DI: Dependency Injection
-    private UserService userService;
+//     //DI: Dependency Injection
+//     private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
 
 
-    @GetMapping("")
+//     @GetMapping("")
     
-    public String getHomePage() {
+//     public String getHomePage() {
 
-        return this.userService.handleHello();
+//         return this.userService.handleHello();
+//     }
+// }
+
+@Controller
+public class UserController {
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "eric.html";
     }
 }
