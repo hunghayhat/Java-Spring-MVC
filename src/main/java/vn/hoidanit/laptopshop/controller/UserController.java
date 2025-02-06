eaddd659cbdc2b8e03bf86a07c1fa41d346216b7
Hung Hay Hat <diegocoshung19@gmail.com>
2025-02-06 21:27:45 +0700

    add model attribute

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,13 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // @RestController
 // public class UserController {
@@ -41,6 +41,18 @@ public class UserController {
         String test = this.userService.handleHello();
         model.addAttribute("eric", test);
         model.addAttribute("hung", "Hello Guys!");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String createUser(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hung) {
+        System.out.println("run here" + hung);
         return "hello";
     }
 }
