79596909b3d4f624ab8c9a3bedeb60bbebddf1c6
hunghayhat <diegocoshung19@gmail.com>
2025-02-21 23:18:54 +0700

    add admin layout

--- src/main/java/vn/hung/laptopshop/controller/UserController.java
+++ src/main/java/vn/hung/laptopshop/controller/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import vn.hung.laptopshop.domain.User;
 import vn.hung.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
